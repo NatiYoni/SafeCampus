1f45abbd680deb9ef5278ca0630da29a5639f347
Natnael Yonas <yonasnatnael11@gmail.com>
2026-01-21 15:59:13 +0300

    feat(mobile/bloc): handle CancelSosEvent in EmergencyBloc

--- mobile/lib/features/emergency/presentation/bloc/emergency_bloc.dart
+++ mobile/lib/features/emergency/presentation/bloc/emergency_bloc.dart
@@ -1,18 +1,29 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../domain/usecases/trigger_sos.dart';
+import '../../domain/usecases/cancel_sos.dart';
 import 'emergency_event.dart';
 import 'emergency_state.dart';
 
 class EmergencyBloc extends Bloc<EmergencyEvent, EmergencyState> {
   final TriggerSos triggerSos;
+  final CancelSos cancelSos;
 
-  EmergencyBloc({required this.triggerSos}) : super(EmergencyInitial()) {
+  EmergencyBloc({required this.triggerSos, required this.cancelSos}) : super(EmergencyInitial()) {
     on<TriggerSosEvent>((event, emit) async {
       emit(EmergencyLoading());
       final result = await triggerSos(event.userId);
       result.fold(
         (failure) => emit(EmergencyError(failure.message)),
         (alert) => emit(SosTriggered(alert)),
+      );
+    });
+
+    on<CancelSosEvent>((event, emit) async {
+      emit(EmergencyLoading());
+      final result = await cancelSos(event.alertId);
+      result.fold(
+        (failure) => emit(EmergencyError(failure.message)),
+        (_) => emit(EmergencyInitial()), // Reset to initial state after cancelling
       );
     });
   }
