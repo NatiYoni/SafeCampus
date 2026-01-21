dfbe8b3cdeb96d8ea3d983616c18ece35754bcba
Natnael Yonas <yonasnatnael11@gmail.com>
2026-01-21 15:59:05 +0300

    feat(mobile/domain): add userName field to Alert entity

--- mobile/lib/features/emergency/domain/entities/alert.dart
+++ mobile/lib/features/emergency/domain/entities/alert.dart
@@ -5,6 +5,7 @@ enum AlertType { sos, manDown, medical, fire, fall }
 class Alert extends Equatable {
   final String id;
   final String userId;
+  final String? userName; // Added userName
   final AlertType type;
   final String status;
   final DateTime timestamp;
@@ -14,6 +15,7 @@ class Alert extends Equatable {
   const Alert({
     required this.id,
     required this.userId,
+    this.userName,
     required this.type,
     required this.status,
     required this.timestamp,
@@ -22,5 +24,5 @@ class Alert extends Equatable {
   });
 
   @override
-  List<Object?> get props => [id, userId, type, status, timestamp, latitude, longitude];
+  List<Object?> get props => [id, userId, userName, type, status, timestamp, latitude, longitude];
 }
