52b8843805193f70763c6fc06d2f5e62610b121f
Natnael Yonas <yonasnatnael11@gmail.com>
2026-01-22 13:23:26 +0300

    feat(mobile): add update profile use case

---
+++ mobile/lib/features/auth/domain/usecases/update_profile.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/error/failures.dart';
+import '../../domain/entities/user.dart';
+import '../repositories/auth_repository.dart';
+
+class UpdateProfile {
+  final AuthRepository repository;
+
+  UpdateProfile(this.repository);
+
+  Future<Either<Failure, User>> call(User user) async {
+    return await repository.updateProfile(user);
+  }
+}
