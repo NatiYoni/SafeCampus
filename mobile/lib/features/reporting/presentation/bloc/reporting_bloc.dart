06c7170406e7726c460072057fece31536e3f06c
Natnael Yonas <yonasnatnael11@gmail.com>
2026-01-22 12:04:35 +0300

    Mobile: Handle ResolveReportRequested event in ReportingBloc to execute usecase

--- mobile/lib/features/reporting/presentation/bloc/reporting_bloc.dart
+++ mobile/lib/features/reporting/presentation/bloc/reporting_bloc.dart
@@ -2,16 +2,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../core/usecases/usecase.dart';
 import '../../domain/usecases/submit_report.dart';
 import '../../domain/usecases/get_reports.dart';
+import '../../domain/usecases/resolve_report.dart';
 import 'reporting_event.dart';
 import 'reporting_state.dart';
 
 class ReportingBloc extends Bloc<ReportingEvent, ReportingState> {
   final SubmitReport submitReport;
-  final GetReports getReports; // Add fetch usecase
+  final GetReports getReports;
+  final ResolveReport resolveReport;
 
   ReportingBloc({
     required this.submitReport,
     required this.getReports,
+    required this.resolveReport,
   }) : super(ReportingInitial()) {
     on<SubmitReportRequested>((event, emit) async {
       emit(ReportingLoading());
@@ -35,5 +38,13 @@ class ReportingBloc extends Bloc<ReportingEvent, ReportingState> {
         (reports) => emit(ReportsLoaded(reports)),
       );
     });
-  }
+    on<ResolveReportRequested>((event, emit) async {
+      // Optimistic update or reload? Let's reload for simplicity or emit loading
+      emit(ReportingLoading());
+      final result = await resolveReport(event.reportId);
+      result.fold(
+        (failure) => emit(ReportingError(message: failure.message)),
+        (_) => add(FetchReportsRequested()), // Refresh
+      );
+    });  }
 }
